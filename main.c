5232a391cfe02427bfb3f7550a1c5c9ec7f84458
Rubcenkov <Rubcenkovvlad977@gmail.com>
2023-09-22 14:28:03 +0300

    Lesson end

---
+++ main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main(){
+    int a,b,c,p,m,u,d;
+    scanf("%d %d %d" , &a,&b,&c);
+    p=a+b;
+    m=a-b;
+    u=a*b;
+    d=a/b;
+    switch(c){
+    case 1: printf("%d",p);break;
+    case 2: printf("%d",m);break;
+    case 3: printf("%d",d);break;
+    case 4: printf("%d",u);break;
+    default:
+    printf("Error");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    
+
+
+
+
+}
